fba84f85b163d4c829411e94304c979323bf4c47
Jérôme Camiré-Bernier <jeromecamirebernier@gmail.com>
2022-09-21 22:06:39 -0400

    creation dun dictio qui contient les labels en strs

--- src_jer/isaid/main_raw_processing.py
+++ src_jer/isaid/main_raw_processing.py
@@ -30,8 +30,18 @@ if __name__ == '__main__':
 
     path_in = r"C:\projets\external\database\isaid\raw-data\dota-v0\\"
     path_out = r"C:\projets\external\database\isaid\data-2022-09-21\data\\"
+    which_seg = "inst"  # inst == instance segmentation, sem == semantic segmentation
 
-    train.move_data_n_labels(path_in=path_in + "train/", path_out=path_out)
+    train.move_data_n_labels(which_seg=which_seg, path_in=path_in + "train/", path_out=path_out + "train/")
+
+
+    # ### fonction poura ller path/train/imgs vs path/val/imgs ###
+
+    with open(path_out + r"\notes.txt", "w", encoding="utf-8") as f:
+        f.write("Le type des labels: {}".format(which_seg))
+        f.write("\n")
+        f.write("Si == inst, alors cela veut dire que les labels sont fait pour de l'instance segmentation.\n")
+        f.write("Si == sem, alors cela veut dire que les labels sont fait pour de la semantic segmentation.")
 
     print('\n')
     print("temps d'exécution: ", datetime.timedelta(seconds=time.time() - time_debut))
--- src_jer/isaid/raw_processing/train.py
+++ src_jer/isaid/raw_processing/train.py
@@ -9,10 +9,12 @@ from pathlib import Path
 import os
 import shutil
 
+import json
+
 
 def move_train_data(path_in, path_out):
 
-    path_out = path_out + "/train/images/"
+    path_out = path_out + "images/"
     Path(path_out).mkdir(parents=True, exist_ok=True)
 
     for i in range(1, 4):
@@ -26,13 +28,72 @@ def move_train_data(path_in, path_out):
                 shutil.move(f, path_out)
 
 
-def move_data_n_labels(path_in, path_out):
+def create_str_segm(annot):
+    if len(annot["segmentation"]) != 1:
+        t = 0
+        raise Exception("pas normal?")
+    str_segm = ""
+    for i in range(len(annot["segmentation"][0])):
+        str_segm = str_segm + str(annot["segmentation"][0][i])
+        if i != len(annot["segmentation"][0]) - 1:
+            str_segm = str_segm + " "
+
+    return str_segm
+
+
+def create_strs_to_file_inst(annots):
+
+    strs_to_file = {}
+    for annot in annots["annotations"]:
+        if annot["image_id"] in strs_to_file:
+            strs_to_file[annot["image_id"]] = strs_to_file[annot["image_id"]] + "\n"
+            str_segm = create_str_segm(annot=annot)
+            strs_to_file[annot["image_id"]] = strs_to_file[annot["image_id"]] + "{} ".format(annot["category_id"]) + str_segm
+        else:
+            strs_to_file[annot["image_id"]] = ""
+            str_segm = create_str_segm(annot=annot)
+            strs_to_file[annot["image_id"]] = strs_to_file[annot["image_id"]] + "{} ".format(annot["category_id"]) + str_segm
+    return strs_to_file
+
+
+def create_labels_in_folder(which_seg, path_in, path_imgs, path_out):
+
+    Path(path_out).mkdir(parents=True, exist_ok=True)
+
+    with open(path_in + "/train-labels/train/Annotations/iSAID_train.json", "r", encoding="utf-8") as f:
+        annots = json.load(f)
+    annots["annotations"] = annots["annotations"][:10]
+
+    if which_seg == "inst":
+        strs_to_file = create_strs_to_file_inst(annots=annots)
+        t = 0
+    elif which_seg == "sem":
+        raise Exception("Pas encore implémenté")
+        path_in = path_in + "/train-labels/train/Semantic_masks/images/images"
+    else:
+        raise Exception("pas normal, c'est soit inst, soit sem, non?")
+
+    img_id_to_img_key = {}
+    for filename in os.listdir(path_imgs):
+        prefix = filename[1:5]
+        path_file = path_out + filename[:-4] + ".txt"
+        key_name = int(prefix)
+        img_id = annots["images"][key_name][
+            "id"]  # f = os.path.join(path_imgs, filename)  # ### checking if it is a file  # if not os.path.isfile(f):  #     raise Exception("pas normal")
+
+    # ### dans la fonction write, sassurer que le .txt nexiste pas, si oui il faut le delete
+
+
+    t = 0
+
+    t = 0
+
+
+def move_data_n_labels(which_seg, path_in, path_out):
 
     move_train_data(path_in=path_in, path_out=path_out)
 
-    path_out = path_out + "/train/labels/"
-    Path(path_out).mkdir(parents=True, exist_ok=True)
-
+    create_labels_in_folder(which_seg=which_seg, path_in=path_in, path_imgs=path_out + "/images/", path_out=path_out + "/labels/")
 
     t = 0
 
